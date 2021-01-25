2372b9419d08a8a809121b1ec2b77beddf4256e5
Ian Farrer <ifarrer@me.com>
2021-01-25 14:25:42 +0100

    changes for -17. options now defined in element modules

--- updaterefs.sh
+++ updaterefs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Script to update the revision section of all of the listed YANG modules with contents of a file. Run with no switches to see the outcome. Run with '-i' to write the changes to the files.
 
-modulelist="ietf-dhcpv6-common.yang ietf-dhcpv6-server.yang ietf-dhcpv6-relay.yang ietf-dhcpv6-client.yang ietf-dhcpv6-options-rfc8415-server.yang ietf-dhcpv6-options-rfc8415-relay.yang ietf-dhcpv6-options-rfc8415-client.yang ietf-example-dhcpv6-options-rfc3319-server.yang ietf-example-dhcpv6-class-selector.yang ietf-example-dhcpv6-server-config.yang"
+modulelist="ietf-dhcpv6-common.yang ietf-dhcpv6-server.yang ietf-dhcpv6-relay.yang ietf-dhcpv6-client.yang ietf-example-dhcpv6-options-rfc3319-server.yang ietf-example-dhcpv6-class-selector.yang ietf-example-dhcpv6-server-config.yang"
 
 if [[ $# -gt 0 ]]
 then
